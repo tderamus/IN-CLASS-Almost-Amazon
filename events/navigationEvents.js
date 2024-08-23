7c7110a68604a8c2f8fbc2445aa161d6ca47a5cb
tderamus <deramust@gmail.com>
2024-08-22 23:01:50 -0500

    added filter for all authors tab on navigation bar

--- events/navigationEvents.js
+++ events/navigationEvents.js
@@ -1,6 +1,8 @@
 import { booksOnSale, getBooks } from '../api/bookData';
+import { favAuthors, getAuthors } from '../api/authorData';
 import { signOut } from '../utils/auth';
 import { showBooks } from '../pages/books';
+import { showAuthors } from '../pages/authors';
 
 // navigation events
 const navigationEvents = () => {
@@ -20,6 +22,14 @@ const navigationEvents = () => {
 
   // FIXME: STUDENTS Create an event listener for the Authors
   // 1. When a user clicks the authors link, make a call to firebase to get all authors
+  document.querySelector('#authors').addEventListener('click', () => {
+    getAuthors().then(showAuthors);
+  });
+
+  // Favorite Authors
+  document.querySelector('#favorite-authors').addEventListener('click', () => {
+    favAuthors().then(showAuthors);
+  });
   // 2. Convert the response to an array because that is what the makeAuthors function is expecting
   // 3. If the array is empty because there are no authors, make sure to use the emptyAuthor function
   document.querySelector('#authors').addEventListener('click', () => {
